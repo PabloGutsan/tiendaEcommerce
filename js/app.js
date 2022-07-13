4ea1d727a2d349e415359d3526ba947f4ca3e231
pablogutsan <pablogutierrez@live.cl>
2022-07-12 21:56:21 -0400

    Despliegue del proyecto

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const modalIgamen = document.querySelector('#modal-imagen');
+
+if(modalIgamen){
+    modalIgamen.addEventListener('show.bs.modal', function(event){
+        const enlace = event.relatedTarget;
+        const rutaImagen = enlace.getAttribute('data-bs-imagen');
+    
+        const imagen = document.createElement('IMG');
+            imagen.src = `img/${rutaImagen}.jpg`;
+            imagen.classList.add('img-fluid');
+            imagen.alt = 'imagen de galeria';
+    
+        const contenidoModal = document.querySelector('.modal-body');
+        contenidoModal.appendChild(imagen);
+    })
+    
+    modalIgamen.addEventListener('hidden.bs.modal', function(){
+        const contenidoModal = document.querySelector('.modal-body');
+        contenidoModal.textContent = '';
+    })
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
